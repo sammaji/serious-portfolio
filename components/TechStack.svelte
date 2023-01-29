3b41ef51081b1af6b62c56462d88d726c460fb6b
Samya brata Maji <samyabratamaji334@gmail.com>
2023-01-29 11:13:29 +0530

    [add] tech stack component

---
+++ components/TechStack.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let icon: string;
+  export let title: string;
+  export let img_to_right: boolean;
+
+  // the img_to_right property will also help us to manage the margins
+  // eg, img --> right, TechStack --> mr-4 (margin-right: 1rem)
+  const flex_margin = img_to_right ? 'ml-4' : 'mr-4';
+</script>
+
+<div
+  class={`h-fit w-fit ${flex_margin} my-2 px-4 py-2 rounded-md bg-slate-200 flex justify-center items-center`}
+>
+  <i class={`icon devicon-${icon} text-3xl pr-1.5`} />
+  <label for="icon">{title}</label>
+</div>
