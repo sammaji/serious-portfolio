59d6f8bd1b047bc38b72dd3824bf81af555f3359
Samya brata Maji <samyabratamaji334@gmail.com>
2023-01-29 11:12:48 +0530

    [add] footer component

---
+++ components/Footer.svelte
@@ -0,0 +1,16 @@
+<script>
+    import FOOTER_DATA from "../src/data/footer.data";
+</script>
+
+<footer class="mt-48 py-24 bg-orange-200 flex flex-wrap justify-evenly">
+    {#each FOOTER_DATA as footer_data}
+    <div>
+        <h4 class="font-bold">{footer_data.heading}</h4>
+        <ul>
+            {#each footer_data.footer as footer_item}
+                <li><a href={footer_item.url}>{footer_item.title}</a></li>
+            {/each}
+        </ul>
+    </div>
+    {/each}
+</footer>
