0b3f4766041b0c162d64778a2855647378515a54
Samya brata Maji <samyabratamaji334@gmail.com>
2023-01-29 11:12:06 +0530

    [add] tech stack data and project data

--- src/data/project.data.ts
+++ src/data/project.data.ts
@@ -2,9 +2,8 @@
  * @param icon favicon
  */
 interface TechStack {
-	title: string;
-	icon: string;
-	icon_style?: string;
+  title: string;
+  icon: string;
 }
 
 /**
@@ -12,35 +11,104 @@ interface TechStack {
  * @param preview_high high quality image lazy loaded or not loaded at all if data-saver is on
  */
 interface ProjectData {
-	title: string;
-	preview_low: string;
-	preview_high: string;
-	description: string;
-	source?: string;
-	demo?: string;
-	tech_stack: TechStack[];
-	tags?: string[];
+  title: string;
+  preview_low: string;
+  preview_high: string;
+  description: string;
+  source?: string;
+  demo?: string;
+  tech_stack: TechStack[];
+  tags?: string[];
 }
 
 /* A full list of technologies I have worked with, in alphabetical order */
 const TECH_STACK = {
-	node: {
-		title: '',
-		icon: '',
-		icon_style: ''
-	}
+  firebase: {
+    title: 'Firebase',
+    icon: 'firebase-plain'
+  },
+  javascript: {
+    title: 'JavaScript',
+    icon: 'javascript-plain'
+  },
+  node: {
+    title: 'Node',
+    icon: 'nodejs-plain'
+  },
+  react: {
+    title: 'React',
+    icon: 'react-original'
+  },
+  threejs: {
+    title: 'ThreeJs',
+    icon: 'threejs-original'
+  }
 };
 
-const PROJECTS_DATA: ProjectData[] = [
-	{
-		title: 'Lab4All',
-		description: 'No desc',
-		preview_low: '',
-		preview_high: '',
-		source: "https://github.com/samyabrata-maji/labforall",
-		demo: "https://lab4all.vercel.app",
-		tech_stack: [TECH_STACK.node]
-	}
+const MY_TECH_STACK: TechStack[] = [
+  TECH_STACK.react,
+  TECH_STACK.threejs,
+  TECH_STACK.firebase,
+  TECH_STACK.node,
+  TECH_STACK.javascript
 ];
 
-export default PROJECTS_DATA;
+const PROJECTS_DATA: ProjectData[] = [
+  {
+    title: 'LabForAll',
+    description:
+      'LabForAll is a website that provides information reguarding practical experiments. It was a hackathon project made for Treasure Hacks 3.0',
+    preview_low:
+      'https://user-images.githubusercontent.com/116789799/215292499-5df7d3ca-e269-48e9-ad1f-a99e5802cd5d.jpg',
+    preview_high:
+      'https://user-images.githubusercontent.com/116789799/215286790-697b0975-0740-487f-90d2-9d9666c6d162.png',
+    source: 'https://github.com/samyabrata-maji/labforall',
+    demo: 'https://lab4all.vercel.app',
+    tech_stack: [
+      TECH_STACK.react,
+      TECH_STACK.threejs,
+      TECH_STACK.firebase,
+      TECH_STACK.node,
+      TECH_STACK.javascript
+    ]
+  },
+  {
+    title: 'LabForAll',
+    description:
+      'LabForAll is a website that provides information reguarding practical experiments. It was a hackathon project made for Treasure Hacks 3.0',
+    preview_low:
+      'https://user-images.githubusercontent.com/116789799/215292499-5df7d3ca-e269-48e9-ad1f-a99e5802cd5d.jpg',
+    preview_high:
+      'https://user-images.githubusercontent.com/116789799/215286790-697b0975-0740-487f-90d2-9d9666c6d162.png',
+    source: 'https://github.com/samyabrata-maji/labforall',
+    demo: 'https://lab4all.vercel.app',
+    tech_stack: [
+      TECH_STACK.react,
+      TECH_STACK.threejs,
+      TECH_STACK.firebase,
+      TECH_STACK.node,
+      TECH_STACK.javascript
+    ]
+  },
+  {
+    title: 'LabForAll',
+    description:
+      'LabForAll is a website that provides information reguarding practical experiments. It was a hackathon project made for Treasure Hacks 3.0',
+    preview_low:
+      'https://user-images.githubusercontent.com/116789799/215292499-5df7d3ca-e269-48e9-ad1f-a99e5802cd5d.jpg',
+    preview_high:
+      'https://user-images.githubusercontent.com/116789799/215286790-697b0975-0740-487f-90d2-9d9666c6d162.png',
+    source: 'https://github.com/samyabrata-maji/labforall',
+    demo: 'https://lab4all.vercel.app',
+    tech_stack: [
+      TECH_STACK.react,
+      TECH_STACK.threejs,
+      TECH_STACK.firebase,
+      TECH_STACK.node,
+      TECH_STACK.javascript
+    ]
+  }
+];
+
+export { PROJECTS_DATA, MY_TECH_STACK };
+export type { TechStack, ProjectData };
